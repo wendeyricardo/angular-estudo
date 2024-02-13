16942d41cc56fa3917f7421a9071ca6e2634eef6
wendey <wendeyricardo@gmail.com>
2024-02-13 01:01:49 -0300

    checklist dialog apagar

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CATEGORY_DATA } from '../category/category.component';
+import { DialogComponent } from '../dialog/dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 export const CHECKLIST_DATA = [
-  { guid: 'aaa-bbb-ccc-ddd', completed: false, description: 'ir ao ortopedista', deadline: Date.now(),
+  { guid: 'aaa-bbb-ccc-ddd', completed: false, description: 'ir ao ortopedista', deadline: Date.now(), postDate: Date.now(),
   category: CATEGORY_DATA.find(x =>x.name=='Saúde')
  },
- { guid: 'aaa-bbb-ccc-ddd', completed: true, description: 'Reunião com o time', deadline: Date.now(),
+ { guid: 'aaa-bbb-ccc-ddd', completed: true, description: 'Reunião com o time', deadline: Date.now(), postDate: Date.now(),
   category: CATEGORY_DATA.find(x =>x.name=='Trabalho')
  },
 ]
@@ -21,13 +23,32 @@ export class ChecklistComponent implements OnInit {
 
   public displayedColumns: string[] = ['id','completed', 'description', 'deadline', 'postDate', 'category', 'actions'];
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
-
+  
   public createNewItem(){
-    console.log('criar novo item do checklist clicado')
+    console.log('criar novo item do checklist clicado');
+  }
+  
+  public updateCompleteStatus(status: boolean){
+    console.log('status alterado $(status)');
+  }
+
+  public updateChecklistItem(checklist: ChecklistComponent){
+    console.log('atualizando item do checklist');
+  }
+
+  public deleteChecklistItem(checklist: ChecklistComponent){
+    console.log('deletando item do checklist');
+
+    this.dialog.open(DialogComponent, {disableClose: true,
+      data: {dialogMsg: 'Você deseja realmente apagar esse item?',
+      leftButtonLabel: 'Cancelar', rightButtonLabel: 'Ok'}
+    }).afterClosed().subscribe(resp => {
+          console.log('janela modal confirmar apagada fechada');
+      });    
   }
 
 }
